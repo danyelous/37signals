5e526f05377ab219bdb5b81e97713042c08e3a81
Daniel Pontoriero <danyelous@gmail.com>
2013-09-24 10:06:42 -0400

    adding additional files

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+//		$('.initialmessage').toggle();
+//		$('.basecamp').toggle();
+
+
+
+    $('.button1').mouseenter(function() {
+		$('.initialmessage').toggle();
+		$('.basecamp').toggle();
+    });
+	$('.button1').mouseleave(function() {
+		$('.initialmessage').toggle();
+		$('.basecamp').toggle();
+    });
+
+    $('.button2').mouseenter(function() {
+		$('.initialmessage').toggle();
+		$('.highrise').toggle();
+    });
+	$('.button2').mouseleave(function() {
+		$('.initialmessage').toggle();
+		$('.highrise').toggle();
+    });
+
+    $('.button3').mouseenter(function() {
+		$('.initialmessage').toggle();
+		$('.campfire').toggle();
+    });
+	$('.button3').mouseleave(function() {
+		$('.initialmessage').toggle();
+		$('.campfire').toggle();
+    });
+
+
+});
